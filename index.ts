037287ed6fa74fe2de09a7f6fa57c9a36c134725
webbegg <webbegg@gmail.com>
2020-05-09 23:19:11 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -2,26 +2,7 @@ import { Card, CardColor, CardValue } from "./components/card"
 
 import './style.scss'
 
-const card1 = new Card('playzone', CardColor.BLUE, CardValue.ONE);
-card1.render()
-
-const card2 = new Card('playzone', CardColor.GREEN, CardValue.TWO);
-card2.render()
-
-const card3 = new Card('playzone', CardColor.RED, CardValue.THREE);
-card3.render()
-
-const card4 = new Card('playzone', CardColor.YELLOW, CardValue.FOUR);
-card4.render()
-
-const card11 = new Card('playzone', CardColor.BLUE, CardValue.ONE);
-card11.render()
-
-const card22 = new Card('playzone', CardColor.GREEN, CardValue.TWO);
-card22.render()
-
-const card33 = new Card('playzone', CardColor.RED, CardValue.THREE);
-card33.render()
-
-const card44 = new Card('playzone', CardColor.YELLOW, CardValue.FOUR);
-card44.render()
+for(let i = 0; i>=12; i++) {
+  const card1 = new Card('playzone', CardColor.BLUE, CardValue.ONE);
+  card1.render()
+}
